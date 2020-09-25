ce2edc7a432022266283ae5ab5060aaf84d877a7
Akinyemi Olumide Emmannuel <akinolu52@gmail.com>
2020-09-25 03:01:18 +0100

    added content type

--- index.ts
+++ index.ts
@@ -3,25 +3,26 @@ import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, AxiosInstance, Me
 export class ApiService {
     private service: AxiosInstance;
 
-    constructor(baseURL: string, token: string) {
-        const localService = axios.create({
+    constructor(baseURL: string, token?: string, contentType?: string) {
+        const instance = axios.create({
             baseURL,
             headers: {
-                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json' || contentType,
                 'Access-Control-Allow-Methods': '*',
                 'Access-Control-Allow-Origin': baseURL,
             },
         });
 
-        localService.interceptors.response.use(this.handleSuccess, this.handleError);
-        localService.interceptors.request.use((config: AxiosRequestConfig) => {
+        instance.interceptors.response.use(this.handleSuccess, this.handleError);
+        instance.interceptors.request.use((config: AxiosRequestConfig) => {
             if (!token) return config;
 
             config.headers['Authorization'] = 'Bearer ' + token;
             return config;
         });
 
-        this.service = localService;
+        this.service = instance;
     }
 
     private handleSuccess = (response: AxiosResponse) => response;
@@ -37,8 +38,9 @@ export class ApiService {
         exectuteWhileLoading?: any,
     ) {
         if (exectuteWhileLoading) exectuteWhileLoading();
+        const requestMethod = method.toLowerCase();
 
-        if (method === 'GET' || method === 'get') {
+        if (requestMethod === 'get' || requestMethod === 'options') {
             return this.service
                 .request({
                     method,
